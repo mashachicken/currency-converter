c477c308e2085638fea3fa731941cc4ef69675ab
masha <mashanechmo@gmail.com>
2021-07-02 11:24:29 -0700

    make API call and add env API keys in env

---
+++ src/currency-service.js
@@ -0,0 +1,15 @@
+
+export default class CurrencyService { 
+  static getCurrency(userSearch) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.currency_API_KEY}/latest/`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return Error(error);
+    })
+}
+}
